13046abdd4c89a74d5ae28d8f71673f5e0d7d4c6
Bhargav Prajapati <Bhargav.Prajapati@rishabhsoft.com>
2024-10-28 16:17:38 +0530

    Initial Commit

---
+++ API/API/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿
+using Core.Entities;
+using Infrastucture.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController (StoreContext context) : ControllerBase
+    {
+        private readonly  StoreContext _Context= context;
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var Products= await _Context.Products.ToListAsync();
+            return Ok(Products);
+        }
+        [HttpGet("{id}")]
+        public  async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var Product = await _Context.Products.FindAsync(id);
+            return Ok(Product);
+        }
+    }
+}
